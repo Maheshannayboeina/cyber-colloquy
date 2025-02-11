401416c1eec3d3c05cbfdd25c0d636a7aed0a1d6
Saahil-23 <saahil.sawant24@gmail.com>
2025-02-11 23:49:54 +0530

    home

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -155,7 +155,7 @@ export function Footer() {
 
         <div className="my-10 text-sm text-center text-gray-600 dark:text-gray-400">
           Copyright © {new Date().getFullYear()} Cyber Security Department
-          | Made with ♥ by{"Mahesh and Saahil"}
+          
         </div>
       </Container>
     </div>
