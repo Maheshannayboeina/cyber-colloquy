ecdb3f27a9b268f1d2d3a6c36a0763510c5ae6f8
Maheshannayboeina <mahesh.annayboeina18426@sakec.ac.in>
2025-03-15 11:26:30 +0530

    award date updated

--- src/app/awards/page.tsx
+++ src/app/awards/page.tsx
@@ -517,7 +517,7 @@ const AwardCeremonyPage: React.FC = () => {
                 </li>
                 <li>
                   <b>Deadline:</b> Ensure your nomination is submitted before
-                  March 15, 2025.
+                  March 20, 2025.
                 </li>{" "}
               </ol>
             </motion.div>
